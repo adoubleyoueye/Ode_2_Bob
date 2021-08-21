13dd20afed469a586be8f01a46e2293478bb38e3
adoubleyoueye <awi.mphaphuli@tuta.io>
2021-08-21 04:09:00 +0200

    Respond with random quote

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 # app.rb
 require "functions_framework"
+require 'json'
+
+quotes = File.read('./quotes.json')
+data_hash = JSON.parse(quotes)
 
 FunctionsFramework.http "bob_says" do |request|
-  "Hello, world!\n"
+  random_int = rand(data_hash.length)
+  data_hash[random_int]
 end
 
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,12 +3,11 @@ require "functions_framework/testing"
 
 describe "functions_bob_says_get" do
   include FunctionsFramework::Testing
-  it "generates the correct response body" do
+  it "generates the correct response status" do
     load_temporary "app.rb" do
       request = make_get_request "http://example.com:8080/"
       response = call_http "bob_says", request
       expect(response.status).to eq 200
-      expect(response.body.join).to eq "Hello, world!\n"
     end
   end
 end
